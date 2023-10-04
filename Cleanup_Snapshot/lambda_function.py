dca998ec853a0eb978a31f87302ae950fd575611
nmphuong200288 <88701458+nmphuong200288@users.noreply.github.com>
2023-10-04 16:44:35 +0000

    Update lambda_function.py

--- Cleanup_Snapshot/lambda_function.py
+++ Cleanup_Snapshot/lambda_function.py
@@ -1,70 +1,27 @@
 import boto3
-conn = boto3.client('rds', region_name='ap-northeast-2')
-ec2 = boto3.client('ec2')
+import dateutil.tz
+from datetime import datetime, timedelta, timezone
 
-conn.create_db_subnet_group(
-    DBSubnetGroupName='databasesubnet',
-    DBSubnetGroupDescription='using-privatesubnet-rds',
-    SubnetIds=[
-        'subnet-0c9f389abf0e6614a',
-        'subnet-064df2573ead56b0f',
-    ],
-    Tags=[
-        {
-            'Key': 'DatabaseRds',
-            'Value': 'mysql8.0'
+region = 'ap-southeast-2'
+
+def lambda_handler(event, lambda_context):
+    ec2 = boto3.resource("ec2", region_name=region)
+    current_date = datetime.now(tz=timezone.utc)
+    ho_chi_minh_tz=dateutil.tz.gettz('Asia/Ho_Chi_Minh')
+    currentdata_gmt7=current_date.astimezone(ho_chi_minh_tz)
+    #diff_date = current_date - timedelta(hours=2)  # timedelta function can be use to date manipulations
+    snapshots = ec2.snapshots.filter(OwnerIds=['self'])
+    currendata_gmt7_str=currentdata_gmt7.strftime('%Y-%m-%d %H:%M:%S')
+    diff_date = currentdata_gmt7 - timedelta(minutes = 43)
+    print(diff_date)
+    for snapshot in snapshots:
+        snapshot_start_time = snapshot.start_time
+         
+        if diff_date > snapshot_start_time:
             
-        },
-    ]
-)
-
-response = ec2.create_security_group(
-    GroupName='MyDBSecurityGroup', 
-    Description='Description for MyDBSecurityGroup', 
-    VpcId='vpc-0bafbe97c1d5315fa',
-    )
-security_group_id = response['GroupId']
-
-ec2.authorize_security_group_ingress(
-    GroupId=security_group_id,
-    IpPermissions=[
-        {
-            'IpProtocol': 'tcp',
-            'FromPort': 3306,
-            'IpRanges': [
-                {
-                    'CidrIp': '0.0.0.0/0',
-                    'Description': 'RDS Access from anywhere',
-                },
-            ],
-            'ToPort': 3306,
-        },
-    ],
-)
-
-
-database = conn.create_db_instance(
-
-        AllocatedStorage=10,
-        DBName="MYRDS",
-        DBInstanceIdentifier="my-first-rds-instance",
-        DBInstanceClass="db.t2.micro",
-        Engine="mysql",
-        MasterUsername="root",
-        MasterUserPassword="pass1234",
-        Port=3306,
-        BackupRetentionPeriod=0,
-        PubliclyAccessible=True,
-        DBSubnetGroupName='databasesubnet',
-        VpcSecurityGroupIds=[response['GroupId']],
-        
-        Tags=[
-            {
-                'Key': 'mydatabase',
-                'Value': 'Mysql'
-            },
-        ],
-        
-    )
-
-print (database)
+            try:
+                snapshot.delete()
+                print("Deleting Snapshot id: ", snapshot.snapshot_id)
+            
+            except Exception as e:
+                print("Current Snapshot is in use: ", snapshot.snapshot_id)
