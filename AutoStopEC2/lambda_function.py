7445eed81227895a66c01cc31df7ad0411e40625
nmphuong200288 <88701458+nmphuong200288@users.noreply.github.com>
2023-10-05 04:04:13 +0000

    Update lambda_function.py

--- AutoStopEC2/lambda_function.py
+++ AutoStopEC2/lambda_function.py
@@ -1,70 +1,19 @@
+import json
 import boto3
-conn = boto3.client('rds', region_name='ap-northeast-2')
-ec2 = boto3.client('ec2')
 
-conn.create_db_subnet_group(
-    DBSubnetGroupName='databasesubnet',
-    DBSubnetGroupDescription='using-privatesubnet-rds',
-    SubnetIds=[
-        'subnet-0c9f389abf0e6614a',
-        'subnet-064df2573ead56b0f',
-    ],
-    Tags=[
+region = 'ap-southeast-2'
+
+def lambda_handler(event, lambda_context):
+    ec2_client = boto3.client("ec2", region_name=region)
+    reservations = ec2_client.describe_instances(Filters=[
         {
-            'Key': 'DatabaseRds',
-            'Value': 'mysql8.0'
-            
-        },
-    ]
-)
+            "Name": 'tag:stopec2',
+            "Values": ['yes'],
+        }
+    ]).get("Reservations")
 
-response = ec2.create_security_group(
-    GroupName='MyDBSecurityGroup', 
-    Description='Description for MyDBSecurityGroup', 
-    VpcId='vpc-0bafbe97c1d5315fa',
-    )
-security_group_id = response['GroupId']
-
-ec2.authorize_security_group_ingress(
-    GroupId=security_group_id,
-    IpPermissions=[
-        {
-            'IpProtocol': 'tcp',
-            'FromPort': 3306,
-            'IpRanges': [
-                {
-                    'CidrIp': '0.0.0.0/0',
-                    'Description': 'RDS Access from anywhere',
-                },
-            ],
-            'ToPort': 3306,
-        },
-    ],
-)
-
-
-database = conn.create_db_instance(
-
-        AllocatedStorage=10,
-        DBName="MYRDS",
-        DBInstanceIdentifier="my-first-rds-instance",
-        DBInstanceClass="db.t2.micro",
-        Engine="mysql",
-        MasterUsername="root",
-        MasterUserPassword="pass1234",
-        Port=3306,
-        BackupRetentionPeriod=0,
-        PubliclyAccessible=True,
-        DBSubnetGroupName='databasesubnet',
-        VpcSecurityGroupIds=[response['GroupId']],
-        
-        Tags=[
-            {
-                'Key': 'mydatabase',
-                'Value': 'Mysql'
-            },
-        ],
-        
-    )
-
-print (database)
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            ec2_client.stop_instances(InstanceIds=[instance_id])
+            print(f"Instances Stopped " + str(instance_id))
